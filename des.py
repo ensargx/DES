1f5141ce008384fe48b41131f8932a7f84c5ad5b
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-02-11 15:17:44 +0300

    fix: key generation fixed

--- des.py
+++ des.py
@@ -264,40 +264,26 @@ class DES:
 
     def generate_subkeys(self):
         # Generate 16 48-bit subkeys using the given 56-bit key
-        return [self._permute(self.initial_key, self._key_permutation_table(i)) for i in range(16)]
+        subkeys = []
+        for i in range(16):
+            # Left shift the key by 1 or 2
+            if i in [0, 1, 8, 15]:
+                self.initial_key = self._left_shift(self.initial_key)
+            else:
+                self.initial_key = self._left_shift(self._left_shift(self.initial_key))
 
-    def _key_permutation_table(self, round):
-        # Key permutation table
-        key_permutation_table = [
-            57, 49, 41, 33, 25, 17, 9,
-            1, 58, 50, 42, 34, 26, 18,
-            10, 2, 59, 51, 43, 35, 27,
-            19, 11, 3, 60, 52, 44, 36,
-            63, 55, 47, 39, 31, 23, 15,
-            7, 62, 54, 46, 38, 30, 22,
-            14, 6, 61, 53, 45, 37, 29,
-            21, 13, 5, 28, 20, 12, 4
-        ]
+            # Permutation choice 2
+            subkey = self._permute(self.initial_key, [
+                14, 17, 11, 24, 1, 5, 3, 28,
+                15, 6, 21, 10, 23, 19, 12, 4,
+                26, 8, 16, 7, 27, 20, 13, 2,
+                41, 52, 31, 37, 47, 55, 30, 40,
+                51, 45, 33, 48, 44, 49, 39, 56,
+                34, 53, 46, 42, 50, 36, 29, 32
+            ])
 
-        # Left shift table
-        left_shift_table = [
-            1, 1, 2, 2, 2, 2, 2, 2,
-            1, 2, 2, 2, 2, 2, 2, 1
-        ]
-
-        # Permute the key using the key permutation table
-        key = self._permute(self.initial_key, key_permutation_table)
-
-        # Split the key into left and right parts
-        left, right = key[:28], key[28:]
-
-        # Left shift the left and right parts
-        for i in range(left_shift_table[round]):
-            left = self._left_shift(left)
-            right = self._left_shift(right)
-
-        # Combine the left and right parts
-        return left + right
+            subkeys.append(subkey)
+        return subkeys
 
 
 if __name__ == "__main__":
